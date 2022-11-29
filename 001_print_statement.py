9c56c64f6937b08c14ba6db1b450ae5ab8c992c3
abukmeil <mohanad.abukmeil@unimi.it>
2022-11-29 10:08:03 +0200

    first version of print statement

---
+++ 001_print_statement.py
@@ -0,0 +1,20 @@
+
+# Single print
+print("Alhamdulillah")
+
+# Blank print
+print("")
+# Multiple separated prints
+print("Alhamdulillah","Mashallah",sep="_")
+
+
+# Multiple separated prints, default is 'end="\n"'
+print("Alhamdulillah","Mashallah",end="\n")
+print()
+
+
+# when 'end=""' there will be no new line in the next print
+print("Alhamdulillah","Mashallah",end="")
+print("Alhamdulillah","Mashallah")
+
+
